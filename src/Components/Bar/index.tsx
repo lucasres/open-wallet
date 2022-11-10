a54a119dfe8d94cdd67a778911ac429e0df5786c
lucasres <lucasresone@gmail.com>
2022-11-09 23:18:09 -0300

    add see effect in bar

--- src/Components/Bar/index.tsx
+++ src/Components/Bar/index.tsx
@@ -1,17 +1,20 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { Text, View } from "react-native";
+import PrivacyContext from "../../Context/PrivacyContext";
 import styles from "./styles";
 
 const Bar: FC = () => {
+    const { see } = useContext(PrivacyContext)
+
     return (
         <View style={styles.container}>
             <View style={styles.wrapper}>
                 <View style={styles.expenses}>
-                    <Text>R$ 1.000,00</Text>
+                    <Text>R$ {see ? '1.000,00' : '*******'}</Text>
                 </View>
-                <Text style={styles.free}>R$ 3.000,00</Text>
+                <Text style={styles.free}>R$ {see ? '3.000,00' : '*******'}</Text>
                 <View style={styles.investment}>
-                    <Text>10%</Text>
+                    <Text>{see ? '10%' : '***'}</Text>
                 </View>
             </View>
         </View>
